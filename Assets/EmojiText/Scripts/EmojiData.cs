3b40944bbb2fa92b268763701fbaa29b5406eddc
DFVSQY <1520018679@qq.com>
2017-02-25 15:32:00 +0800

    增添了一个填充EmojiData数据的脚本工具

--- Assets/EmojiText/Scripts/EmojiData.cs
+++ Assets/EmojiText/Scripts/EmojiData.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 #if UNITY_EDITOR
 using UnityEditor;
+using System.IO;
 public class EmojiEditor
 {
-    [MenuItem("GameObject/UI/EmojiText",priority = 0)]
+    [MenuItem("GameObject/UI/EmojiText", priority = 0)]
     public static void CreateEmojiText()
     {
         Transform par = null;
@@ -24,8 +25,29 @@ public class EmojiEditor
         RectTransform rt = go.transform as RectTransform;
         rt.localScale = Vector3.one;
         rt.localPosition = Vector3.zero;
-        rt.sizeDelta = new Vector3(100f,100f);
+        rt.sizeDelta = new Vector3(100f, 100f);
         EditorGUIUtility.PingObject(go);
+    }
+
+    [MenuItem("EmojiHelper/FillEmojiData")]
+    public static void FillEmojiData()
+    {
+        EmojiData ed = Resources.Load<EmojiData>("EmojiData/EmojiData");
+        if(ed == null)
+        {
+            Debug.LogError("请首先在工程视图中的EmojiData文件夹下创建EmojiData文件,选中EmojiData文件夹右键点击Create/CreateEmojiData即可");
+            return;
+        }
+        ed.datas.Clear();
+        string[] files = Directory.GetFiles(Application.dataPath + "/EmojiText/Emojis", "*.png");
+        foreach(string file in files)
+        {
+            string key = "[" + Path.GetFileNameWithoutExtension(file) + "]";
+            string p = file.Replace(Application.dataPath, "Assets");
+            Sprite s = AssetDatabase.LoadAssetAtPath<Sprite>(p);
+            ed.datas.Add(new EmojiSprites() { key = key, sprite = s });
+        }
+        AssetDatabase.Refresh();
     }
 }
 #endif
@@ -37,7 +59,7 @@ public struct EmojiSprites
     public Sprite sprite;
 }
 
-[CreateAssetMenu(fileName = "EmojiData",menuName = "CreateEmojiData",order = 1)]
+[CreateAssetMenu(fileName = "EmojiData", menuName = "CreateEmojiData", order = 1)]
 public class EmojiData : ScriptableObject
 {
     public List<EmojiSprites> datas = new List<EmojiSprites>();
